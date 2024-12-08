928de3241f5a50316ffdc999380cf42031630d03
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-12-08 23:21:49 +0530

    Create Form1.cs

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             String user = Convert.ToString(textBox1.Text);
+            String password = Convert.ToString(textBox2.Text);
+            if (user == "imashi" && password == "0000")
+            {
+                Form2 mm = new Form2();
+                mm.Show();
+                this.Hide();
+            }
+            else
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+             textBox1.Clear();
+            textBox2.Clear();
+        }
+        }
+    }
+
